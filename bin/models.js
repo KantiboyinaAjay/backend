d76ed651ede8b0e5c3a3bb0c4919293a3dc17b31
kantiboyinaAjay <ajaykantiboyina12389@gmail.com>
2024-08-20 14:58:37 +0530

    d

---
+++ bin/models.js
@@ -0,0 +1,43 @@
+const { type } = require('@testing-library/user-event/dist/type');
+const mongoose = require('mongoose');
+
+const Questions_table = new mongoose.Schema({
+    test_cases : {
+        type :String,
+        // required : true
+    },
+    title : { 
+        type: String,
+         unique :true,
+        // required: true,
+
+    },
+    Topic_difficulty :{
+        type: String,
+        // required: true,
+
+    },
+    URL :{ 
+        type : String,
+        // required : true,
+    },
+    description:{
+        type : String,
+        // required : true,
+    },
+    constraints:{
+        type: Number,
+        // required : true,
+    },
+    sample_input:{
+        type:String,
+        // required : true,
+    },
+    sample_output:{
+        type:String,
+        // required : true    
+    }
+});
+
+const Questions_Table = mongoose.model("Questions_table",Questions_table);
+module.exports = {Questions_Table};
